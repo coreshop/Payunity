d776e1f5ba2a4273b5c95d87697a9d3a90fddb3c
dpfaffenbauer <dominik@lineofcode.at>
2016-05-25 16:13:30 +0200

    forward payment validate action to CheckoutController

--- views/scripts/payment/validate.php
+++ views/scripts/payment/validate.php
@@ -1,29 +1,24 @@
-<div class="container shop checkout checkout-step-5">
-
-    <?=$this->partial("coreshop/helper/order-steps.php", array("step" => 5));?>
-
-    <form action="<?=$this->module->getPaymentUrl()?>" method="post">
-        <div class="panel panel-smart">
-            <div class="panel-heading">
-                <h3 class="panel-title"><?=$this->translate("PayUnity")?></h3>
-            </div>
-            <div class="panel-body delivery-options">
-                <p><?=sprintf($this->translate("Do you want to pay the amount %s with PayUnity?", \CoreShop\Tool::formatPrice($this->cart->getTotal())))?></p>
-
-                <div class="row">
-                    <div class="col-xs-12">
-                        <a href="<?=$this->url(array("lang" => $this->language, "act" => "payment"), "coreshop_checkout")?>" class="btn btn-default pull-left">
-                            <?=$this->translate("Back")?>
-                        </a>
-
-                        <button type="submit" class="btn btn-white btn-borderd pull-right">
-                            <?=$this->translate("Submit Payment")?>
-                        </button>
-                    </div>
-                </div>
-
-            </div>
+<form action="<?=$this->module->getPaymentUrl()?>" method="post">
+    <div class="panel panel-smart">
+        <div class="panel-heading">
+            <h3 class="panel-title"><?=$this->translate("PayUnity")?></h3>
         </div>
+        <div class="panel-body delivery-options">
+            <p><?=sprintf($this->translate("Do you want to pay the amount %s with PayUnity?", \CoreShop\Tool::formatPrice($this->cart->getTotal())))?></p>
 
-    </form>
-</div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <a href="<?=$this->url(array("lang" => $this->language, "act" => "payment"), "coreshop_checkout")?>" class="btn btn-default pull-left">
+                        <?=$this->translate("Back")?>
+                    </a>
+
+                    <button type="submit" class="btn btn-white btn-borderd pull-right">
+                        <?=$this->translate("Submit Payment")?>
+                    </button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</form>
