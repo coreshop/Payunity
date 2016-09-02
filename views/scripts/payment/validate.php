d74420d9aaa13ba02da6279c03d917c1b853d51b
dpfaffenbauer <dominik@lineofcode.at>
2016-09-02 11:44:34 +0200

    refactor tools class to be override-able

--- views/scripts/payment/validate.php
+++ views/scripts/payment/validate.php
@@ -4,7 +4,7 @@
             <h3 class="panel-title"><?=$this->translate("PayUnity")?></h3>
         </div>
         <div class="panel-body delivery-options">
-            <p><?=sprintf($this->translate("Do you want to pay the amount %s with PayUnity?", \CoreShop\Tool::formatPrice($this->cart->getTotal())))?></p>
+            <p><?=sprintf($this->translate("Do you want to pay the amount %s with PayUnity?", \CoreShop::getTools()->formatPrice($this->cart->getTotal())))?></p>
 
             <div class="row">
                 <div class="col-xs-12">
