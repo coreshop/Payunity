a26fdd9a28c7114a053a2332d7ffb42282581113
Stefan Hagspiel <shagspiel@dachcom.ch>
2016-09-26 14:55:09 +0200

    fix cart object in paymentreturn

--- controllers/PaymentController.php
+++ controllers/PaymentController.php
@@ -62,7 +62,7 @@ class Payunity_PaymentController extends Payment
 
             if ($cart instanceof \CoreShop\Model\Cart) {
                 if (strstr($returnvalue, "ACK")) {
-                    $order = $this->cart->createOrder(\CoreShop\Model\Order\State::getById(\CoreShop\Model\Configuration::get("SYSTEM.ORDERSTATE.PAYMENT")), $this->getModule(), $this->cart->getTotal(), $this->view->language);
+                    $order = $cart->createOrder(\CoreShop\Model\Order\State::getById(\CoreShop\Model\Configuration::get("SYSTEM.ORDERSTATE.PAYMENT")), $this->getModule(), $cart->getTotal(), $this->view->language);
 
                     $payments = $order->getPayments();
 
